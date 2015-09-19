dd5652084e44be516190b8434fd1c12f6ba83826
chltnwjd7399@naver.com <chltnwjd7399@naver.com>
2015-09-19 18:36:13 +0900

    09.19 xml 수정
    1. Singup.xml TableLayout으로 수정
    2. 액션바 활성화 대신 Layout을 이용한 바 추가
    3. edit_card_name.xml 레이아웃 수정 + 포커스 커서 위치 개선
    4. 카드 표시 레이아웃 개선 및 바코드 이미지 사이즈 확대
    5. 카드 추가시 포커스 순서 순차적으로 이동하도록 개선
    [ xml 단에서 수정시는 아이디 선언 문제가 있으므로 java 단에서 뷰 정보를 수정하였음]

--- 4Dpay/app/src/main/java/com/project4D/fdpay/ShowCardInfoActivity.java
+++ 4Dpay/app/src/main/java/com/project4D/fdpay/ShowCardInfoActivity.java
@@ -33,8 +33,8 @@ public class ShowCardInfoActivity extends ActionBarActivity {
 
         MultiFormatWriter gen = new MultiFormatWriter();
         try {
-            final int WIDTH = 320;
-            final int HEIGHT = 180;
+            final int WIDTH = 960;
+            final int HEIGHT = 540;
             BitMatrix bytemap = gen.encode(cm.getCardNumById(bundle.getInt("ID")), BarcodeFormat.CODE_128, WIDTH, HEIGHT);
             Bitmap bitmap = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
             for (int i = 0 ; i < WIDTH ; ++i)
