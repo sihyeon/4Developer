6210c8e34dde33253aa1f3d08ea641720d77284d
sihyeon <ehpoos92@hanmail.net>
2015-10-03 18:56:00 +0900

    코드 정리 및 주석 첨부 완료

--- Tizen/developer/.build/Result Resource Layer/js/circle-helper.js
+++ Tizen/developer/.build/Result Resource Layer/js/circle-helper.js
@@ -11,6 +11,46 @@
 		headerCollapseHandler = [],
 		i, len;
 
+(function() {
+		//화면 꺼짐 방지
+		tizen.power.request("SCREEN", "SCREEN_DIM");
+		
+		var page = document.getElementById( "main" );
+		var text = document.getElementById("maintext");
+		var changer = document.getElementById( "hsectionchanger" );
+		var s_pointX, s_pointY, e_pointX, e_pointY, lengthX, lengthY;
+		var idx=1;
+		console.log("javascript loaded");
+
+		page.addEventListener( "touchstart", function(e){
+			s_pointX = e.touches[0].clientX;
+			s_pointY = e.touches[0].clientY;
+		});
+
+		page.addEventListener( "touchmove", function(e){
+			e_pointX = e.touches[0].clientX;
+			e_pointY = e.touches[0].clientY;
+		});
+
+		page.addEventListener( "touchend", function(){
+			lengthX = (s_pointX > e_pointX)?s_pointX-e_pointX:e_pointX-s_pointX;
+			lengthY = (s_pointY > e_pointY)?s_pointY-e_pointY:e_pointY-s_pointY;
+
+			// SAP 연결 확인
+			if(!SASocket){
+				//SAP 연결 안되어있으면 연결
+				connect();
+				console.log("reconnect");
+				dispPopup("핸드폰과 연결되지 않았습니다."+'</br>'+"연결을 진행합니다.");
+			}
+			
+			//터치 분석
+			else(lengthX + lengthY <= 60){
+				//touch
+				console.log("touched");
+				controlPage("touch");
+			}		
+			
 	if (tau.support.shape.circle) {
 		document.addEventListener("pagebeforeshow", function (e) {
 			page = e.target;
